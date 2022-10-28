eb8250b3839b3873ac89855947a07943e2b3d4da
Damodar <dhegde50@gmail.com>
2022-10-28 17:36:08 -0500

    footer fix

--- src/components/AlwaysHomeFooter/AlwaysHomeFooter.js
+++ src/components/AlwaysHomeFooter/AlwaysHomeFooter.js
@@ -6,21 +6,21 @@ const AlwaysHomeFooter = (props) => {
     return (
         <footer id="sticky-footer" class=" bg-dark text-white-50 mt-5">
         <div class="container text-center">
-        <ul class="nav justify-content-center">
+        <ul class="nav d-block justify-content-center">
             <li class="nav-item">
-                <a href="#" class="nav-link text-white">Contact: <strong>+1 (123) 456-7890</strong></a>
+                <a href="#" class="nav-link p-1 text-white">Contact: <strong>+1 (123) 456-7890</strong></a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link text-white">FAQs</a>
+                <a href="#" class="nav-link p-1 text-white">FAQs</a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link text-white">Careers</a>
+                <a href="#" class="nav-link p-1 text-white">Careers</a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link text-white">All rights reserved</a>
+                <a href="#" class="nav-link p-1 text-white">All rights reserved</a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link text-white">Terms and Conditions</a>
+                <a href="#" class="nav-link p-1 text-white">Terms and Conditions</a>
             </li>
         </ul>
         </div>
