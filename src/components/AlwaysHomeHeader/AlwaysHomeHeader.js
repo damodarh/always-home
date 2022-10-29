09c802ef4efbe00e30c5d580a56f313931de1ca7
Damodar <dhegde50@gmail.com>
2022-10-29 18:32:01 -0500

    Fixed nav button and made app more responsive

--- src/components/AlwaysHomeHeader/AlwaysHomeHeader.js
+++ src/components/AlwaysHomeHeader/AlwaysHomeHeader.js
@@ -8,11 +8,11 @@ const AlwaysHomeHeader = (props) => {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="a"><img src="img/AH.png" className='logo-image' alt="logo" />
-                    </a><button className="navbar-toggler" type="button" data-mdb-toggle="collapse"
-                        data-mdb-target="#navbarExample01" aria-controls="navbarExample01" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <i className="fas fa-bars"></i>
+                    <a className="navbar-brand" href="a">
+                        <img src="img/AH.png" className='logo-image' alt="logo" />
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                     {/* <div className="collapse navbar-collapse" id="navbarExample01">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -25,8 +25,8 @@ const AlwaysHomeHeader = (props) => {
                         </ul>
                     </div> */}
                     <div><Search searchText={props.searchText} handleInputChange={props.handleInputChange} /></div>
-                    <div className="container-fluid">
-                        <ul className="nav justify-content-end menu-list">
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
                                 <a className="nav-link text-black" href="#">
                                     <svg xmlns="http://www.w3.org/2000/svg"
@@ -45,7 +45,7 @@ const AlwaysHomeHeader = (props) => {
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link active text-black" aria-current="page" href="#">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
                                         <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
                                     </svg>
