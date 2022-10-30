e2ef767ba70b6cc27128f5def19d29a7bd9c5404
Damodar <dhegde50@gmail.com>
2022-10-30 14:07:30 -0500

    Footer changes

--- src/components/AlwaysHomeFooter/AlwaysHomeFooter.js
+++ src/components/AlwaysHomeFooter/AlwaysHomeFooter.js
@@ -5,26 +5,22 @@ const AlwaysHomeFooter = (props) => {
 
     return (
         <footer id="sticky-footer" className=" bg-dark text-white-50 mt-5 sticky-bottom">
-        <div className="container text-center">
-        <ul className="nav justify-content-center">
-            <li className="nav-item">
-                <a href="#" className="nav-link p-1 text-white">Contact: <strong>+1 (123) 456-7890</strong></a>
-            </li>
-            <li className="nav-item">
-                <a href="#" className="nav-link p-1 text-white">FAQs</a>
-            </li>
-            <li className="nav-item">
-                <a href="#" className="nav-link p-1 text-white">Careers</a>
-            </li>
-            <li className="nav-item">
-                <a href="#" className="nav-link p-1 text-white">All rights reserved</a>
-            </li>
-            <li className="nav-item">
-                <a href="#" className="nav-link p-1 text-white">Terms and Conditions</a>
-            </li>
-        </ul>
-        </div>
-      </footer>
+            <div className="container text-center">
+                <ul className="nav justify-content-center">
+                    <li className="nav-item">
+                        <a href="#" className="nav-link p-1 text-white">&copy; 2022 Always Home, Inc</a>
+                    </li>
+                    <span className='pl-1 pr-1'>.</span>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link p-1 text-white">Privacy</a>
+                    </li>
+                    <span className='pl-1 pr-1'>.</span>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link p-1 text-white">Terms</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
     )
 
 }
