4c30a4e65a79e76ad3f240c2c55d75bbcd5dd510
Damodar <dhegde50@gmail.com>
2022-10-29 20:30:33 -0500

    Sections added

--- src/components/PropertyDetail/PropertyDetail.js
+++ src/components/PropertyDetail/PropertyDetail.js
@@ -7,23 +7,25 @@ const ProeprtyDetail = (props) => {
 
     return (
         <div className='property-detail'>
-            <section className='mb-3'>
-                <h2 className='property-title'>{property.title}</h2>
-                <div>
-                    <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            className="bi bi-star-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                        </svg>
-                        <span className='ms-1'>{property.rating.toFixed(2)}</span>
+            <div>
+                <section className='mb-3'>
+                    <h2 className='property-title'>{property.title}</h2>
+                    <div>
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                className="bi bi-star-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                            </svg>
+                            <span className='ms-1'>{property.rating.toFixed(2)}</span>
+                            <span className='dot'>.</span>
+                            <span><u>{property.reviews} reviews</u></span>
+                        </span>
                         <span className='dot'>.</span>
-                        <span><u>{property.reviews} reviews</u></span>
-                    </span>
-                    <span className='dot'>.</span>
-                    <span><u>{property.city}, {property.state}, {property.country}</u></span>
-                </div>
-            </section>
+                        <span><u>{property.city}, {property.state}, {property.country}</u></span>
+                    </div>
+                </section>
+            </div>
             <div class="row">
                 <div class="col-lg-6" >
                     <img src={`img/${property.images[0]}`} className='rounded title-image' />
@@ -36,6 +38,27 @@ const ProeprtyDetail = (props) => {
                             </div>
                         })}
                     </div>
+                </div>
+            </div>
+            <div className='prop-detail-section'>
+                <section>
+                    <div>
+                        <h2 className='property-host'>
+                            Entire home hosted by XYZ
+                        </h2>
+                    </div>
+                    <ul class="list-group list-group-horizontal">
+                        <li class="list-group-item border-0 ps-0">{`${property.bedroom} bedroom${property.bedroom > 1 ? 's' : ''}`}</li>
+                        <li class="list-group-item border-0 ps-0">{`${property.beds} bed${property.beds > 1 ? 's' : ''}`}</li>
+                        <li class="list-group-item border-0 ps-0">{`${property.bath} bath${property.bath > 1 ? 's' : ''}`}</li>
+                    </ul>
+                </section>
+            </div>
+            <div className='prop-detail-section'>
+                <div>
+                    <h2 className='property-host'>
+                        Amenities
+                    </h2>
                 </div>
             </div>
         </div>
