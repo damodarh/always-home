dc8ed0e06878902dfa98b41ce0982d0c857067a0
Damodar <dhegde50@gmail.com>
2022-10-30 23:26:07 -0500

    Changed favorites icon

--- src/components/AlwaysHomeHeader/AlwaysHomeHeader.js
+++ src/components/AlwaysHomeHeader/AlwaysHomeHeader.js
@@ -39,7 +39,9 @@ const AlwaysHomeHeader = (props) => {
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-black" href="#"><i className="fa fa-star"></i>
+                                <a className="nav-link text-black" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+                                </svg>
                                     <span className='ms-2'>Favorites</span>
                                 </a>
                             </li>
