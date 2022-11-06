855caa835ad75a687459ed9cf7f5c69a91f60066
Damodar <dhegde50@gmail.com>
2022-11-05 21:58:42 -0500

    Added express mongoose mongo connectivity code and default schema generator for mongo

---
+++ models/Property.js
@@ -0,0 +1,63 @@
+const mongoose = require(mongoose);
+
+const PropertySchema = new mongoose.Schema({
+    title: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    state: {
+        type: String
+    },
+    country: {
+        type: String
+    },
+    host: {
+        type: String
+    },
+    reviews: {
+        type: Number
+    },
+    pricePerNight: {
+        type: Number
+    },
+    cleaningFee: {
+        type: Number
+    },
+    serviceFee: {
+        type: Number
+    },
+    amenities: {
+        type: Array
+    },
+    bedroom: {
+        type: Number
+    },
+    beds: {
+        type: Number
+    },
+    bath: {
+        type: Number
+    },
+    images: {
+        type: Array
+    },
+    rating: {
+        type: Number
+    },
+    avgCost: {
+        type: Number
+    },
+    availability: {
+        type: String
+    },
+    distance: {
+        type: String
+    },
+    favorite: {
+        type: Boolean
+    }
+});
+
+module.exports = Property = mongoose.model('property', PropertySchema);
---
+++ mongoSchemaGenerator.js
@@ -0,0 +1,32 @@
+const generateSchema = require('generate-schema');
+
+let jsonData = {
+    title: '',
+    city: '',
+    state: '',
+    country: '',
+    host: '',
+    reviews: 0,
+    pricePerNight: 0,
+    cleaningFee: 0,
+    serviceFee: 0,
+    amenities: [],
+    bedroom: 0,
+    beds: 0,
+    bath: 0,
+    images: [],
+    rating: 0,
+    avgCost: 0,
+    availability: '',
+    distance: '',
+    favorite: false
+};
+
+let mongooseSchema = generateSchema.mongoose(jsonData);
+
+console.log(JSON
+    .stringify(mongooseSchema, null, " ")
+    .replaceAll(
+        "{,\n",
+        "},\n"
+    ).replaceAll('\"', ''));
