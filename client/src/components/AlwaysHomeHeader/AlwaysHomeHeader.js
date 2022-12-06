bc1fc78f95fa0a32b990a7c32bf8a33e78377999
Damodar <dhegde50@gmail.com>
2022-12-05 19:45:13 -0600

    Header and footer changes

--- client/src/components/AlwaysHomeHeader/AlwaysHomeHeader.js
+++ client/src/components/AlwaysHomeHeader/AlwaysHomeHeader.js
@@ -1,63 +1,112 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 import Search from '../Search/Search';
 import './AlwaysHomeHeader.css';
 
-const AlwaysHomeHeader = (props) => {
+const AlwaysHomeHeader = ({
+    searchText,
+    handleInputChange,
+    auth: { isAuthenticated, loading },
+    logout,
+}) => {
+    const authLinks = (
+        <ul className='navbar-nav ms-auto'>
+            <li className='nav-item'>
+                <Link className='nav-link text-black' to='/profile'>
+                <i class="fa fa-thin fa-user"></i>{' '}
+                    <span className='ms-2'>Profile</span>
+                </Link>
+            </li>
+            <li className='nav-item'>
+                <Link className='nav-link text-black' to='/favorites'>
+                <i class="fa fa-thin fa-star"></i>{' '}
+                    <span className='ms-2'>Favorites</span>
+                </Link>
+            </li>
+            <li className='nav-item'>
+                <Link
+                    className='nav-link active text-black'
+                    aria-current='page'
+                    to='/login'
+                    onClick={logout}
+                >
+                    <i className='fa fa-light fa-arrow-right-to-bracket fa-rotate-180'></i>{' '}
+                    <span className='ms-2'>Logout</span>
+                </Link>
+            </li>
+        </ul>
+    );
+
+    const guestLinks = (
+        <ul className='navbar-nav ms-auto'>
+            <li className='nav-item'>
+                <Link className='nav-link text-black' to='/register'>
+                    <i className='fas fa-thin fa-user-plus'></i>{' '}
+                    <span className='ms-2'>Register</span>
+                </Link>
+            </li>
+            <li className='nav-item'>
+                <Link
+                    className='nav-link active text-black'
+                    aria-current='page'
+                    to='/login'
+                >
+                    <i class="fas fa-light fa-arrow-right-to-bracket"></i>{' '}
+                    <span className='ms-2'>Login</span>
+                </Link>
+            </li>
+        </ul>
+    );
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="a">
-                        <img src="img/AH.png" className='logo-image' alt="logo" />
-                    </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+            <nav className='navbar navbar-expand-lg navbar-light bg-white border-bottom'>
+                <div className='container-fluid'>
+                    <Link className='navbar-brand' to='a'>
+                        <img
+                            src='img/AH.png'
+                            className='logo-image'
+                            alt='logo'
+                        />
+                    </Link>
+                    <button
+                        className='navbar-toggler'
+                        type='button'
+                        data-bs-toggle='collapse'
+                        data-bs-target='#navbarText'
+                        aria-controls='navbarText'
+                        aria-expanded='false'
+                        aria-label='Toggle navigation'
+                    >
+                        <span className='navbar-toggler-icon'></span>
                     </button>
-                    {/* <div className="collapse navbar-collapse" id="navbarExample01">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item active">
-                                <a className="nav-link" aria-current="page" href="#"><strong>Home</strong></a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">About</a>
-                            </li>
-                        </ul>
-                    </div> */}
-                    <div><Search searchText={props.searchText} handleInputChange={props.handleInputChange} /></div>
-                    <div className="collapse navbar-collapse" id="navbarText">
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item">
-                                <a className="nav-link text-black" href="#">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
-                                    </svg>
-                                    <span className='ms-2'>Profile</span>
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link text-black" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                                </svg>
-                                    <span className='ms-2'>Favorites</span>
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link active text-black" aria-current="page" href="#">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
-                                        <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
-                                        <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
-                                    </svg>
-                                    <span className='ms-2'>Logout</span>
-                                </a>
-                            </li>
-                        </ul>
+                    {isAuthenticated && <div>
+                        <Search
+                            searchText={searchText}
+                            handleInputChange={handleInputChange}
+                        />
+                    </div>}
+                    <div className='collapse navbar-collapse' id='navbarText'>
+                        {!loading && (
+                            <>{isAuthenticated && authLinks}</>
+                        )}
                     </div>
                 </div>
             </nav>
         </>
-    )
+    );
+};
 
-}
+AlwaysHomeHeader.propTypes = {
+    logout: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+};
 
-export default AlwaysHomeHeader;
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logout })(AlwaysHomeHeader);
--- client/src/components/AlwaysHomeModal/AlwaysHomeModal.js
+++ client/src/components/AlwaysHomeModal/AlwaysHomeModal.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 import './AlwaysHomeModal.scss';
 
 const AlwaysHomeModal = (props) => {
-
     return (
-        <div className="modal fade" id="detailModal" data-bs-keyboard="true" tabIndex="-1" aria-labelledby="detailModalLabel" aria-hidden="true">
-            <div className={`${props.modalDialogClassName} modal-lg modal-dialog-centered`}>
+        <div
+            className='modal fade'
+            id={props.id}
+            data-bs-keyboard='true'
+            tabIndex='-1'
+            aria-labelledby={props.label}
+            aria-hidden='true'
+        >
+            <div
+                className={`${props.modalDialogClassName} modal-lg modal-dialog-centered`}
+            >
                 <div className={props.modalContentClassName}>
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="detailModalLabel">{props.modalTitle}</h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <div className='modal-header'>
+                        <h5 className='modal-title' id={props.label}>
+                            {props.modalTitle}
+                        </h5>
+                        <button
+                            type='button'
+                            className='btn-close'
+                            data-bs-dismiss='modal'
+                            aria-label='Close'
+                        ></button>
                     </div>
                     <div className={props.modalBodyClassName}>
                         {props.children}
@@ -17,7 +33,16 @@ const AlwaysHomeModal = (props) => {
                 </div>
             </div>
         </div>
-    )
+    );
+};
+
+AlwaysHomeModal.propTypes = {
+    modalTitle: PropTypes.string.isRequired,
+    modalDialogClassName: PropTypes.string,
+    modalBodyClassName: PropTypes.string,
+    modalContentClassName: PropTypes.string,
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
 };
 
 export default AlwaysHomeModal;
