5a5a00e0f06297754dbae30f27f2d35de4ecbcde
Damodar Hegde <dhegde50@gmail.com>
2022-12-10 02:32:50 -0600

    Started profile component

--- client/src/components/Layout/Profile.js
+++ client/src/components/Layout/Profile.js
@@ -1,7 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Profile = (props) => {
-  return <div></div>;
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/properties").then((resp) => setProperties(resp.data));
+  }, []);
+
+  console.log(properties);
+
+  return (
+    <div>
+      <ul className='list-group list-group-horizontal'>
+        {properties.map((property, index) => {
+          return (
+            <li clasName='list-group-item d-flex justify-content-between align-items-center' key={index}>
+              <div>
+                <span>{property.title}</span>
+                <button className='btn btn-primary btn-sm'>
+                  Update property details
+                </button>
+                <button className='btn btn-danger btn-sm'>
+                  Take property off market
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Profile;
